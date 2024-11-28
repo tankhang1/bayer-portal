367fb188929bdc0a36a11fb65d465b7f00d84bd1
Ethan <doank3442@gmail.com>
2024-11-29 02:49:58 +0700

    refactor/link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { store } from "@/redux/store";
 import { redirect } from "next/navigation";
 export default function Home() {
   const isAuth = store.getState().app.token ? true : false;
-  if (isAuth) redirect("/dashboard/sales");
+  if (isAuth) redirect("/iqr/iqr-list");
   else redirect("/auth/signin/basic");
 }
