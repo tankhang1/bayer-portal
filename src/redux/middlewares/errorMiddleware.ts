237d6f8eaa77255285e51d9ff6f793506b78d136
Ethan <doank3442@gmail.com>
2025-03-03 21:31:46 +0700

    refactor/error-middleware

--- src/redux/middlewares/errorMiddleware.ts
+++ src/redux/middlewares/errorMiddleware.ts
@@ -1,6 +1,7 @@
 // import { Alert } from 'react-native';
 import type { Middleware } from "@reduxjs/toolkit";
 import { isRejectedWithValue } from "@reduxjs/toolkit";
+import { redirect } from "next/navigation";
 
 export const rtkQueryErrorLogger: Middleware =
   () => (next) => async (action: any) => {
